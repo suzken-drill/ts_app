42f8bfe295e7b67d4ffcc00b8df7b6ebc142697e
suzken-drill <suzken@wcf-drill.com>
2019-12-17 21:02:08 +0900

    create items any method

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,23 +1,44 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_admin!
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    redirect_to items_path, flash: {error: I18n.t(:item_not_exist_error, scope: :message)}
+  end
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+
   def index
+    @items = Item.page(params[:page]).per(Item::PER)
   end
 
   def show
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to item_path(@item), flash: { notice: I18n.t(:create_finish_message, scope: :message) }
+    else
+      render action: :new, flash: { error: I18n.t(:create_error_message, scope: :message) }
+    end
   end
 
   def edit
   end
 
   def update
+    if @item.update(item_params)
+      redirect_to item_path(@item), flash: { notice: I18n.t(:update_finish_message, scope: :message) }
+    else
+      render action: :edit, flash: { error: I18n.t(:update_error_message, scope: :message) }
+    end
   end
 
   def destroy
+    @item.destroy
+    redirect_to items_path, flash: { notice: I18n.t(:destroy_finish_message, scope: :message) }
   end
 
   def file_upload
@@ -28,4 +49,11 @@ class ItemsController < ApplicationController
     
   end
 
+  private
+    def set_item
+      @item = Item.find(params[:id])
+    end
+    def item_params
+      params.require(:item).permit(Item::REGISTRABLE_ATTRIBUTES)
+    end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,16 +4,24 @@ class Item < ApplicationRecord
 	has_one :category, through: :item_category_relation
 
 	# constant
-
+	REGISTRABLE_ATTRIBUTES = %i(item_number title price content content_simple image_url sale_start_at image_1 image_2 image_3 image_4 image_5 image_6 wholesale_price postage include_devision packing_size shipment_from shipment_to strick_price lower_price)
+	PER = 10
 
 	# validate
-	validates :item_number, presence: { message: I18n.t(:item_number_blank_error) }
-	validates :title, presence: { message: I18n.t(:name_blank_error) }
-	validates :price, presence: { message: I18n.t(:price_blank_error) }
-	validates :content, presence: { message: I18n.t(:content_blank_error) }
-	validates :wholesale_price, prisence: { message: I18n.t(:wholesale_price_blank_error) }
+	validates :item_number, presence: { message: I18n.t(:item_number_blank_error, scope: [:activerecord, :errors, :models, :item, :attributes, :item_number]) }
+	validates :title, presence: { message: I18n.t(:title_blank_error, scope: [:activerecord, :errors, :models, :item, :attributes, :title]) }
+	validates :price, presence: { message: I18n.t(:price_blank_error, scope: [:activerecord, :errors, :models, :item, :attributes, :price]) }, numericality: { only_integer: { message: I18n.t(:price_only_integer) }, greater_than: 0 }
+	validates :content, presence: { message: I18n.t(:content_blank_error, scope: [:activerecord, :errors, :models, :item, :attributes, :content]) }
+	validates :wholesale_price, presence: { message: I18n.t(:wholesale_price_blank_error, scope: [:activerecord, :errors, :models, :item, :attributes, :wholesale_price]) }
+	validate :price_check
 
 	# scope
 	scope :with_stock, -> { includes(:stock) }
 
+	def price_check
+		return true unless self.price.present? && self.wholesale_price.present?
+		if self.price < self.wholesale_price
+			errors.add(:price, I18n.t(:greater_than_wholesale_price, scope: [:activerecord, :errors, :models, :item, :attributes, :price]))
+		end
+	end
 end
